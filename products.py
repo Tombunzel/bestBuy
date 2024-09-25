11754f5fbb63ae46909fe3d1d80396682beb976c
Tombunzel <bunzel.tom@gmail.com>
2024-09-25 14:24:31 +0200

    Created Product class

---
+++ products.py
@@ -0,0 +1,34 @@
+class Product():
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        if quantity == 0:
+            self.active = False
+        self.quantity = quantity
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        return f"{self.name}, Price: ${self.price}, Quantity: {self.quantity}"
+
+    def buy(self, quantity):
+        if self.quantity >= quantity:
+            self.quantity -= quantity
+            return f"Total: {self.price * quantity}"
+        raise Exception("Couldn't complete purchase, not enough items in stock.")
+
+
