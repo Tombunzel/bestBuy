24d1a8f519f90bed125d310ad79f3dd49db974c1
Tombunzel <bunzel.tom@gmail.com>
2024-09-29 18:20:07 +0200

    Implementing getter, setter and staticmethod and improving variable naming

--- store.py
+++ store.py
@@ -1,39 +1,49 @@
 class Store:
     """class Store for handling purchases and quantities etc."""
-    def __init__(self, list_of_products):
+
+    def __init__(self, products):
         """constructor"""
-        self.list_of_products = list_of_products
+        self._products = products
+
+    @property
+    def products(self):
+        return self._products
+
+    @products.setter
+    def products(self, new_products):
+        self._products = new_products
 
     def add_product(self, product):
         """adds a product to list of products"""
-        self.list_of_products.append(product)
+        self._products.append(product)
 
     def remove_product(self, product):
         """removes product from list of products"""
-        self.list_of_products.remove(product)
+        self._products.remove(product)
 
-    def get_total_quantity(self):
+    def get_total_stock(self):
         """returns the total amount of items in store"""
-        total_quantity = 0
-        for product in self.list_of_products:
-            total_quantity += product.quantity
-        return total_quantity
+        total_stock = 0
+        for product in self._products:
+            total_stock += product.quantity
+        return total_stock
 
     def get_all_products(self):
         """returns a list of all active products in store"""
         all_products = []
-        for product in self.list_of_products:
+        for product in self._products:
             if product.is_active():
                 all_products.append(product)
         return all_products
 
+    @staticmethod
+    def order(shopping_list):
+        """returns total price of order"""
+        total = 0
+        for product, quantity in shopping_list:
+            current_total = product.buy(quantity)
+            total += current_total
+        return total
 
-def order(shopping_list):
-    """returns total price of order"""
-    total = 0
-    for product, quantity in shopping_list:
-        current_total = product.buy(quantity)
-        if current_total is None:
-            return None
-        total += current_total
-    return total
+    def __contains__(self, item):
+        return item in self.products
